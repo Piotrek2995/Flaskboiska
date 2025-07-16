cd07ce5a3b69166afe3b69d69522771453ee1d5e
pp_adm <piotr.pawlus@poczta.onet.pl>
2025-07-16 12:51:33 +0200

    1ZMIANY

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ login_manager.login_view = 'login'
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.Text, nullable=False)
+
 
 class Match(db.Model):
     id = db.Column(db.Integer, primary_key=True)
